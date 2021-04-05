633ee327763e68fcb3812f47f408ca9acbf7eb59
raghavavamseedharrachuru <raghavavamseedhar@gmail.com>
2021-04-05 12:44:19 +0530

    Part 3: Solution

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -11,7 +11,7 @@ public class Item {
         return name;
     }
 
-//    public int getPrice() {return price;}
+    public int getPrice() {return price;}
 
     @Override
     public String toString(){
